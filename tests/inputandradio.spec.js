2c18e581aec94d5b76a315731b91aa4424005a94
Gilberto R Rose <gilbertorosester@gmail.com>
2026-01-21 20:49:41 -0500

    none

--- tests/inputandradio.spec.js
+++ tests/inputandradio.spec.js
@@ -1,7 +1,17 @@
 const { test, expect } = require('@playwright/test');
 
-test('example test', async ({ page }) => {
+test('inputandradio', async ({ page }) => {
   await page.goto('http://uitestingplayground.com/textinput');
+
+  
+    //await page.getByRole('textbox').check();
+    await page.check('button');
+  await page.getByRole('textbox').fill("Hello World");
+  await page.getByRole('button').click();
+  
+  //await page.getByRole('button')
+  
+  await page.waitForTimeout(5000);
   
 
   page.close();

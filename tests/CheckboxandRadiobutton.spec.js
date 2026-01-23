4e2cac7f0f72b8610c889bfec15f0a6b256940f9
Gilberto R Rose <gilbertorosester@gmail.com>
2026-01-22 19:44:47 -0500

    none

--- tests/CheckboxandRadiobutton.spec.js
+++ tests/CheckboxandRadiobutton.spec.js
@@ -2,7 +2,17 @@ const {test, expect} = require ('@playwright/test');
 
 test('Check box and Radio Button', async ({page})=>{
 
-    
+await page.goto('https://proleed.academy/exercises/selenium/automation-practice-form-with-radio-button-check-boxes-and-drop-down.php');
 
+await page.getByRole('textbox', { name: /First Name/i }).fill("Gilberto");
+await page.getByRole('textbox', { name: /Last Name/i }).fill("Rose");
+
+await page.waitForTimeout(5000);
+
+// await page.locator("#passport").check();
+
+expect(await page.locator("#passport").isCheckedChecked()).toBeFalsy();
+
+await page.waitForTimeout(5000);
 
 })

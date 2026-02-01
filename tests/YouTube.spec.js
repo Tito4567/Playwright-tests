9db63e13f068566b8b78fe3f4800b965416752ab
Gilberto R Rose <gilbertorosester@gmail.com>
2026-02-01 10:26:07 -0500

    nothing

--- tests/YouTube.spec.js
+++ tests/YouTube.spec.js
@@ -5,4 +5,15 @@ test('Playing with YouTube', async ({page})=>{
     await page.goto('https://www.youtube.com/post/UgkxGEk7SFTEH1YXlVbtDoex_OAuxdf9fATx');
 
   //await page.waitForTimeout(10000);
-})
+  Tito();
+}
+
+
+
+);
+
+function Tito()
+{
+  console.log("Hello world");
+}
+

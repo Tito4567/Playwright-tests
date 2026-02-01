25c376f06daaf72396c0496a743ca0cfd8013591
Gilberto R Rose <gilbertorosester@gmail.com>
2026-01-31 22:40:55 -0500

    Modify YouTube test to include function call
    Commented out a timeout and added a function call.

--- tests/YouTube.spec.js
+++ tests/YouTube.spec.js
@@ -4,12 +4,14 @@ test('Playing with YouTube', async ({page})=>{
 
     await page.goto('https://www.youtube.com/post/UgkxGEk7SFTEH1YXlVbtDoex_OAuxdf9fATx');
 
-  //await page.waitForTimeout(10000);  
+  //await page.waitForTimeout(10000);
+func();
+    
 }
 
 function func()
 {
-    
+    Console.log("Hello World");
 }
 
 )

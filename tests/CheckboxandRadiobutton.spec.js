d5edea8d100b55b90a20f5ac6da4ca02bcbf2d32
Gilberto R Rose <gilbertorosester@gmail.com>
2026-01-31 21:04:05 -0500

    none

--- tests/CheckboxandRadiobutton.spec.js
+++ tests/CheckboxandRadiobutton.spec.js
@@ -10,11 +10,14 @@ await page.getByRole('textbox', { name: /First Name/i }).fill("Gilberto");
 await page.getByRole('textbox', { name: /Last Name/i }).fill("Rose");
 await page.locator('[name="fathername"]').fill("Gilberto R. Rose Sr");
 await page.locator('[name="mothername"]').fill("Nelva C. Rose Sr");
+await page.locator("#identity_number").fill("123456");
+
 expect (await page.getByLabel('Saving').isVisible());
 console.log(await page.getByLabel('Saving').isChecked()+" "+"is value");
 await page.getByLabel('Pension').click();
 expect (await page.getByLabel('Saving').isChecked());
-await page.waitForTimeout(10000);
+
+//await page.waitForTimeout(10000);
 
 expect(await page.locator("#passport").isVisible());
 expect(await page.locator('#studentid').isVisible());
@@ -29,6 +32,6 @@ expect(await page.locator('#drivinglicense').isVisible());
 
 expect(await page.locator("#passport")).toBeChecked();
 
-//await page.waitForTimeout(5000);
+await page.waitForTimeout(10000);
 
 })
